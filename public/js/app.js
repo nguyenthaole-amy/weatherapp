717929a174616db14f1b2a55d9e8d862456ea51d
nguyenthaole-amy <le46t@mtholyoke.edu>
2022-01-09 12:58:43 -0500

    Remove console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,6 @@
-console.log('Client side js file is loaded')
-
 // fetch('url').then((response) => {
 //     response.json().then((data) => {
 //         console.log(data)
-//     })
-// })
-
-// fetch('http://localhost:3000/weather?address=!').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error)
-//         } else {
-//             console.log(data.location)
-//             console.log(data.forecast)
-//         }
 //     })
 // })
 
